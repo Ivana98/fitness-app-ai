e00435d6de9c840c7b38f09ee9a19c2ffb10fa02
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-02-09 20:00:10 +0100

    dodat main

--- FitnessAppAi/src/test_model.py
+++ FitnessAppAi/src/test_model.py
@@ -8,24 +8,28 @@ IMG_W, IMG_H = 100, 100
 NUM_OF_CLASSES = 131
 BATCH_SIZE = 32
 
-BASE_PATH = "D:/Fakultet/7sms-Soft_computing"
+BASE_PATH = "D:/Semestar7/Soft kompjuting/Projekat"
 TEST_DIR = BASE_PATH + "/fruits-360/Test"
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
-test_generator = test_datagen.flow_from_directory(
-    TEST_DIR,
-    target_size=(IMG_W, IMG_H),
-    batch_size=BATCH_SIZE,
-    class_mode='categorical')
+def main():
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_generator = test_datagen.flow_from_directory(
+        TEST_DIR,
+        target_size=(IMG_W, IMG_H),
+        batch_size=BATCH_SIZE,
+        class_mode='categorical')
 
-# Recreate the exact same model, including its weights and the optimizer
-loaded_model = keras.models.load_model('../saved_models/saved_modelsmodel1.h5')
+    # Recreate the exact same model, including its weights and the optimizer
+    loaded_model = keras.models.load_model('../saved_models/saved_modelsmodel1.h5')
 
-# Show the model architecture
-loaded_model.summary()
+    # Show the model architecture
+    loaded_model.summary()
 
-# Show results
-test_score = loaded_model.evaluate_generator(test_generator, verbose=0)
+    # Show results
+    test_score = loaded_model.evaluate_generator(test_generator, verbose=0)
 
-print('Test loss:', test_score[0])
-print('Test accuracy:', test_score[1])
+    print('Test loss:', test_score[0])
+    print('Test accuracy:', test_score[1])
+
+if __name__ == '__main__':
+    main()
